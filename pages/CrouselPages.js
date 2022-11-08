2bc2621ec15ed224b8ea8733c5c84149e84301bd
Brielt Bella Gracitwo <44756651+EGraci@users.noreply.github.com>
2022-11-09 01:52:32 +0700

    onBoarding

--- pages/CrouselPages.js
+++ pages/CrouselPages.js
@@ -1,21 +1,73 @@
-// import { View, Text, StyleSheet } from 'react-native'
-// import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { withTheme } from '@rneui/themed';
 
-// export default function CrouselPages({data}) {
-//   return (
-//     <View style={{flexDirection:'row', height:64 }}>
-//       {
-//         data.map((_, i)
-
-//         )
-//       }
-//     </View>
-//   )
-// }
-// const styles = StyleSheet.create({
-//     container:{
-//       flex: 1,
-//       justifyContent: "center",
-//       alignItems: "center",
-//     }
-//   });
+export default function CrouselPages({data, pageIndex, scrollTo, scrollEnd}) {
+    
+  return (
+    <View style={styles.body}>
+        <View style={styles.paggingBar}>
+        {
+            data.map((_, i) => {
+            return <View style={[styles.paggingItem, pageIndex == i &&{backgroundColor: '#7DE5ED', width: 30}]} key={i.toString()} />
+            })
+        }
+        </View>
+        {
+            pageIndex == data.length - 1 ?
+                <View style={styles.pageButton}>
+                    <TouchableOpacity style={{backgroundColor: '#7743DB', flex:1, alignItems: "center", padding: 10,}}>
+                        <Text style={{color: 'white', fontWeight: 'bold'}}>Mulai</Text>
+                    </TouchableOpacity>
+                </View>
+            :
+                <View style={styles.pageButton}>
+                    <TouchableOpacity style={[styles.footerButton, {borderWidth: 1, borderColor: '#7743DB'}]} onPress={scrollEnd} >
+                        <Text>Lewati</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[styles.footerButton, {backgroundColor: '#7743DB'}]} onPress={scrollTo}>
+                        <Text style={{color: 'white', fontWeight: 'bold'}}>Lanjut</Text>
+                    </TouchableOpacity>
+                </View>
+        }
+    </View>
+    )
+}
+const styles = StyleSheet.create({
+    container:{
+        flexDirection: 'row',
+        justifyContent: "center",
+        alignItems: "center",
+    }, 
+    body:{
+        flex: 1, 
+        width: "100%",
+        flexDirection: 'column',
+        position: 'absolute',
+        bottom: 20,
+    },
+    paggingBar:{
+        flexDirection: 'row',
+        justifyContent: "center",
+    },
+    paggingItem:{
+        height: 2.5,
+        width: 20,
+        backgroundColor: '#7743DB',
+        marginHorizontal: 8,
+        borderRadius: 2,
+    },
+    pageButton:{
+        top: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+    },
+    footerButton:{
+        flex:0.5,
+        alignItems: "center",
+        padding: 10,
+    }
+  });
--- pages/CrouselScreen.js
+++ pages/CrouselScreen.js
@@ -1,15 +1,37 @@
 import { View, Text, FlatList, StyleSheet, Animated } from 'react-native'
-import React, {useState, useRef} from 'react'
+import React, { useRef } from 'react';
 import slides from '../data/Sildes';
 import CrouselItem from '../pages/CrouselItem';
 import CrouselPages from './CrouselPages';
+import { Dimensions } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
 export default function CrouselScreen() {
+  const [pageIndex, setPageIndex] = React.useState(0);
+  const width = Dimensions.get('window').width;
+  const slideRef = useRef(null);
+  const setCurrentIndex = e => {
+      const contentOffsetX = e.nativeEvent.contentOffset.x;
+      setPageIndex(Math.round(contentOffsetX / width));
+  };
+  const nextTo = () => {
+    if(pageIndex < slides.length){
+        slideRef.current.scrollToIndex({index: pageIndex + 1});
+        setPageIndex(pageIndex + 1);
+    }else{
+        console.log("end");
+    }
+  };
 
+  const skipTo = () =>{
+    slideRef.current.scrollToIndex({index: slides.length - 1});
+    setPageIndex(slides.length - 1);
+  };
   return (
     <View style={styles.container}>
       <View style={{flex: 3}}>
       <FlatList 
+      onMomentumScrollEnd={setCurrentIndex}
         data={slides} 
         renderItem={
             ({item}) => 
@@ -23,9 +45,11 @@ export default function CrouselScreen() {
         panGestureHandlerProps={{
           activeOffsetX: [-10, 10],
         }}
+        keyExtractor={(item) => item.id}
+        ref={slideRef}
         />
       </View>
-      {/* <CrouselPages data={slides} /> */}
+      <CrouselPages data={slides} pageIndex={pageIndex} scrollTo={nextTo} scrollEnd={skipTo}/>
     </View>
   )
 }
