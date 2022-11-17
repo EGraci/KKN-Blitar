fb49fcc3ba6338867f0f81fbe419a09168d61057
BenedictoReinaldy <reinaldybenedict13@gmail.com>
2022-11-18 00:23:38 +0700

    masukkan data dan list asset done
    update di asset menambahkan folder foto berwarna, foto hp (hitam putih), dan video. already listed in folder data

---
+++ data/FotoBerwarna.js
@@ -0,0 +1,72 @@
+export default[
+    {
+        id : 1,
+        title : 'Agus Salim',
+        image: require('../assets/fotoberwarna/agussalim1.jpg'),
+    },
+    {
+        id : 2,
+        title : 'Ahmad Yani',
+        image: require('../assets/fotoberwarna/ahmadyani1.jpg'),
+    },
+    {
+        id : 3,
+        title : 'Bung Tomo',
+        image: require('../assets/fotoberwarna/bungtomo1.jpg'),
+    },
+    {
+        id : 4,
+        title : 'Cut Nyak Dien',
+        image: require('../assets/fotoberwarna/cutnyakdien1.jpg'),
+    },
+    {
+        id : 5,
+        title : 'Ir. Soekarno',
+        image: require('../assets/fotoberwarna/irsoekarno1.jpg'),
+    },
+    {
+        id : 6,
+        title : 'Ki Hadjar Dewantara',
+        image: require('../assets/fotoberwarna/kihadjardewantara1.jpg'),
+    },
+    {
+        id : 7,
+        title : 'Martha Christina Tiahahu',
+        image: require('../assets/fotoberwarna/martha1.jpg'),
+    },
+    {
+        id : 8,
+        title : 'Mohammad Hatta',
+        image: require('../assets/fotoberwarna/mohhatta1.jpg'),
+    },
+    {
+        id : 9,
+        title : 'Muhammad Yamin',
+        image: require('../assets/fotoberwarna/muhyamin1.jpg'),
+    },
+    {
+        id : 10,
+        title : 'Raden Ajeng Kartini',
+        image: require('../assets/fotoberwarna/rakartini1.jpg'),
+    },
+    {
+        id : 11,
+        title : 'Wage Rudolf Supratman',
+        image: require('../assets/fotoberwarna/rudolf1.jpg'),
+    },
+    {
+        id : 12,
+        title : 'Raden Soedirman',
+        image: require('../assets/fotoberwarna/soedirman1.jpg'),
+    },
+    {
+        id : 13,
+        title : 'Sutan Syahrir',
+        image: require('../assets/fotoberwarna/syahrir1.jpg'),
+    },
+    {
+        id : 14,
+        title : 'HOS Tjokroaminoto',
+        image: require('../assets/fotoberwarna/tjokroaminoto1.jpg'),
+    },
+]
---
+++ data/Video.js
@@ -0,0 +1,7 @@
+export default[
+    {
+        id : 1,
+        title : 'Sutan Syahrir',
+        video: require('../assets/video/sutansyahrir.mp4'),
+    },
+];
