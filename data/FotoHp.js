be3a76fb7bb4b3c269a34ff9391dfa7b98e1fd3f
Emanuel1009 <73428926+Emanuel1009@users.noreply.github.com>
2022-11-24 22:42:39 +0700

    Update Dataset Foto

--- data/FotoHp.js
+++ data/FotoHp.js
@@ -1,72 +1,72 @@
-export default[
-    {
-        id : 1,
-        title : 'Agus Salim',
-        image: require('../assets/fotoberwarna/agussalim2.jpg'),
-    },
-    {
-        id : 2,
-        title : 'Ahmad Yani',
-        image: require('../assets/fotoberwarna/ahmadyani2.jpg'),
-    },
-    {
-        id : 3,
-        title : 'Bung Tomo',
-        image: require('../assets/fotoberwarna/bungtomo2.jpg'),
-    },
-    {
-        id : 4,
-        title : 'Cut Nyak Dien',
-        image: require('../assets/fotoberwarna/cutnyakdien2.jpg'),
-    },
-    {
-        id : 5,
-        title : 'Ir. Soekarno',
-        image: require('../assets/fotoberwarna/irsoekarno2.jpg'),
-    },
-    {
-        id : 6,
-        title : 'Ki Hadjar Dewantara',
-        image: require('../assets/fotoberwarna/kihadjardewantar2.jpg'),
-    },
-    {
-        id : 7,
-        title : 'Martha Christina Tiahahu',
-        image: require('../assets/fotoberwarna/martha2.jpg'),
-    },
-    {
-        id : 8,
-        title : 'Mohammad Hatta',
-        image: require('../assets/fotoberwarna/mohhatta2.jpg'),
-    },
-    {
-        id : 9,
-        title : 'Muhammad Yamin',
-        image: require('../assets/fotoberwarna/muhyamin2.jpg'),
-    },
-    {
-        id : 10,
-        title : 'Raden Ajeng Kartini',
-        image: require('../assets/fotoberwarna/rakartini2.jpg'),
-    },
-    {
-        id : 11,
-        title : 'Wage Rudolf Supratman',
-        image: require('../assets/fotoberwarna/rudolf2.jpg'),
-    },
-    {
-        id : 12,
-        title : 'Raden Soedirman',
-        image: require('../assets/fotoberwarna/soedirman2.jpg'),
-    },
-    {
-        id : 13,
-        title : 'Sutan Syahrir',
-        image: require('../assets/fotoberwarna/syahrir2.jpg'),
-    },
-    {
-        id : 14,
-        title : 'HOS Tjokroaminoto',
-        image: require('../assets/fotoberwarna/tjokroaminoto2.jpg'),
-    },
-]
+export default [
+  {
+    id: 1,
+    title: "Agus Salim",
+    image: require("../assets/fotohp/agussalim2.jpg"),
+  },
+  {
+    id: 2,
+    title: "Ahmad Yani",
+    image: require("../assets/fotohp/ahmadyani2.jpg"),
+  },
+  {
+    id: 3,
+    title: "Bung Tomo",
+    image: require("../assets/fotohp/bungtomo2.jpg"),
+  },
+  {
+    id: 4,
+    title: "Cut Nyak Dien",
+    image: require("../assets/fotohp/cutnyakdien2.jpg"),
+  },
+  {
+    id: 5,
+    title: "Ir. Soekarno",
+    image: require("../assets/fotohp/irsoekarno2.jpg"),
+  },
+  {
+    id: 6,
+    title: "Ki Hadjar Dewantara",
+    image: require("../assets/fotohp/kihadjardewantar2.jpg"),
+  },
+  {
+    id: 7,
+    title: "Martha Christina Tiahahu",
+    image: require("../assets/fotohp/martha2.jpg"),
+  },
+  {
+    id: 8,
+    title: "Mohammad Hatta",
+    image: require("../assets/fotohp/mohhatta2.jpg"),
+  },
+  {
+    id: 9,
+    title: "Muhammad Yamin",
+    image: require("../assets/fotohp/muhyamin2.jpg"),
+  },
+  {
+    id: 10,
+    title: "Raden Ajeng Kartini",
+    image: require("../assets/fotohp/rakartini2.jpg"),
+  },
+  {
+    id: 11,
+    title: "Wage Rudolf Supratman",
+    image: require("../assets/fotohp/rudolf2.jpg"),
+  },
+  {
+    id: 12,
+    title: "Raden Soedirman",
+    image: require("../assets/fotohp/soedirman2.jpg"),
+  },
+  {
+    id: 13,
+    title: "Sutan Syahrir",
+    image: require("../assets/fotohp/syahrir2.jpg"),
+  },
+  {
+    id: 14,
+    title: "HOS Tjokroaminoto",
+    image: require("../assets/fotohp/tjokroaminoto2.jpg"),
+  },
+];
