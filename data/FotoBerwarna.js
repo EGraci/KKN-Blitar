ac67985e5ed2540eff3ef4b5434031e3f2aee1d0
Brielt Bella Gracitwo <44756651+EGraci@users.noreply.github.com>
2022-11-25 22:21:53 +0700

    change jpg to png

--- data/FotoBerwarna.js
+++ data/FotoBerwarna.js
@@ -2,12 +2,12 @@ export default[
     {
         id : 1,
         title : 'Agus Salim',
-        image: require('../assets/fotoberwarna/agussalim1.jpg'),
+        image: require('../assets/fotoberwarna/agussalim1.png'),
     },
     {
         id : 2,
         title : 'Ahmad Yani',
-        image: require('../assets/fotoberwarna/ahmadyani1.jpg'),
+        image: require('../assets/fotoberwarna/ahmadyani1.png'),
     },
     {
         id : 3,
@@ -17,7 +17,7 @@ export default[
     {
         id : 4,
         title : 'Cut Nyak Dien',
-        image: require('../assets/fotoberwarna/cutnyakdien1.jpg'),
+        image: require('../assets/fotoberwarna/cutnyakdien1.png'),
     },
     {
         id : 5,
@@ -27,7 +27,7 @@ export default[
     {
         id : 6,
         title : 'Ki Hadjar Dewantara',
-        image: require('../assets/fotoberwarna/kihadjardewantara1.jpg'),
+        image: require('../assets/fotoberwarna/kihadjardewantara1.png'),
     },
     {
         id : 7,
@@ -37,36 +37,36 @@ export default[
     {
         id : 8,
         title : 'Mohammad Hatta',
-        image: require('../assets/fotoberwarna/mohhatta1.jpg'),
+        image: require('../assets/fotoberwarna/mohhatta1.png'),
     },
     {
         id : 9,
         title : 'Muhammad Yamin',
-        image: require('../assets/fotoberwarna/muhyamin1.jpg'),
+        image: require('../assets/fotoberwarna/muhyamin1.png'),
     },
     {
         id : 10,
         title : 'Raden Ajeng Kartini',
-        image: require('../assets/fotoberwarna/rakartini1.jpg'),
+        image: require('../assets/fotoberwarna/rakartini1.png'),
     },
     {
         id : 11,
         title : 'Wage Rudolf Supratman',
-        image: require('../assets/fotoberwarna/rudolf1.jpg'),
+        image: require('../assets/fotoberwarna/rudolf1.png'),
     },
     {
         id : 12,
         title : 'Raden Soedirman',
-        image: require('../assets/fotoberwarna/soedirman1.jpg'),
+        image: require('../assets/fotoberwarna/soedirman1.png'),
     },
     {
         id : 13,
         title : 'Sutan Syahrir',
-        image: require('../assets/fotoberwarna/syahrir1.jpg'),
+        image: require('../assets/fotoberwarna/syahrir1.png'),
     },
     {
         id : 14,
         title : 'HOS Tjokroaminoto',
-        image: require('../assets/fotoberwarna/tjokroaminoto1.jpg'),
+        image: require('../assets/fotoberwarna/tjokroaminoto1.png'),
     },
 ]
--- pages/PreviewScreen.js
+++ pages/PreviewScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, Button, StyleSheet, useWindowDimensions } from 'react-native'
+import { View, Text, Button, StyleSheet } from 'react-native'
 import React, { useRef } from 'react';
 import FotoBerwarna from '../data/FotoBerwarna';
 import FotoHp from '../data/FotoHp';
@@ -6,26 +6,14 @@ import FotoHp from '../data/FotoHp';
 
 export default function PreviewScreen({navigation, route}) {
     const { pahlawan, tipe } = route.params;
-    const {width} = useWindowDimensions();
-
+    const data = FotoBerwarna.find(el => el.title.includes === pahlawan);
+    console.debug(data);
     return (
       <View style={styles.container}>
         <Button title={'Kembali ke Scan'} onPress={() => navigation.navigate('Onboard', null)} />
-
+        
         <Text> dengan tipe {tipe}</Text>
-        {
-            (() => {
-                if(tipe.includes('clr')){
-                    const data = FotoBerwarna.find(el => el.title == pahlawan);
-                    return(<Image source={data.image} style={[styles.image, {width, resizeMode: 'contain'}]} />);
-                }else if(tipe.includes('hp')){
-                    const data = FotoHp.find(el => el.title == pahlawan);
-                    return(<Image source={data.image} style={[styles.image, {width, resizeMode: 'contain'}]} />);
-                }else{
-                    return(<Text>Preview Video Pahlawan {pahlawan}</Text>);
-                }
-            })()
-        }
+       
       </View>
     )
   }
@@ -37,9 +25,6 @@ export default function PreviewScreen({navigation, route}) {
       backgroundColor: "white",
     },
     image:{
-        width: 270,
-        height: 270,
         justifyContent: 'center',
-        marginTop: 20,
     },
   });
