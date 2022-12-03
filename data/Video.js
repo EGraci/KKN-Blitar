a650bfb26cc8fc78f4821c340ed812178c2571b6
BenedictoReinaldy <reinaldybenedict13@gmail.com>
2022-12-04 00:42:21 +0700

    update script

--- data/Video.js
+++ data/Video.js
@@ -1,7 +1,77 @@
-export default[
-    {
-        id : 1,
-        title : 'Sutan Syahrir',
-        video: require('../assets/video/sutansyahrir.mp4'),
-    },
+export default [
+  {
+    id: 1,
+    title: "Agus Salim",
+    image: require("../assets/fotoberwarna/agussalim-video.mp4"),
+  },
+  // {
+  //   id: 2,
+  //   title: "Ahmad Yani",
+  //   image: require("../assets/fotoberwarna/ahmadyani1.png"),
+  // },
+  {
+    id: 3,
+    title: "Bung Tomo",
+    image: require("../assets/fotoberwarna/bungtomo-video.mp4"),
+  },
+  // {
+  //   id: 4,
+  //   title: "Cut Nyak Dien",
+  //   image: require("../assets/fotoberwarna/cutnyakdien1.png"),
+  // },
+  {
+    id: 5,
+    title: "Ir. Soekarno",
+    image: require("../assets/fotoberwarna/soekarno-video"),
+  },
+  {
+    id: 6,
+    title: "Ki Hadjar Dewantara",
+    image: require("../assets/fotoberwarna/kihajardewantara-video.mp4"),
+  },
+  {
+    id: 7,
+    title: "Martha Christina Tiahahu",
+    image: require("../assets/fotoberwarna/martha-video"),
+  },
+  {
+    id: 8,
+    title: "Mohammad Hatta",
+    image: require("../assets/fotoberwarna/mohhatta-video.mp4"),
+  },
+  {
+    id: 9,
+    title: "Mohammad Yamin",
+    image: require("../assets/fotoberwarna/mohyamin-video.mp4"),
+  },
+  {
+    id: 10,
+    title: "Raden Ajeng Kartini",
+    image: require("../assets/fotoberwarna/kartini-video.mp4"),
+  },
+  {
+    id: 11,
+    title: "Wage Rudolf Supratman",
+    image: require("../assets/fotoberwarna/wrsupratman-video.mp4"),
+  },
+  {
+    id: 12,
+    title: "Raden Soedirman",
+    image: require("../assets/fotoberwarna/soedirman-video.mp4"),
+  },
+  {
+    id: 13,
+    title: "Sutan Syahrir",
+    image: require("../assets/fotoberwarna/sutansyahrir-video.mp4"),
+  },
+  {
+    id: 14,
+    title: "HOS Tjokroaminoto",
+    image: require("../assets/fotoberwarna/tjokroaminoto-video.mp4"),
+  },
+  // {
+  //   id: 15,
+  //   title: "Abdul Haris Nasution",
+  //   image: require("../assets/fotoberwarna/abdulharis1.png"),
+  // },
 ];
