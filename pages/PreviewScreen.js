b7e9c86f85c7e5ce87f1818fb10f0b363940a89e
Brielt Bella Gracitwo <44756651+EGraci@users.noreply.github.com>
2022-12-04 23:13:12 +0700

    update data set foto

--- pages/PreviewScreen.js
+++ pages/PreviewScreen.js
@@ -1,12 +1,16 @@
-import { View, Text, Button, StyleSheet } from "react-native";
+import { View, Text, Button, StyleSheet, Image, useWindowDimensions} from "react-native";
 import React, { useRef } from "react";
 import FotoBerwarna from "../data/FotoBerwarna";
 import FotoHp from "../data/FotoHp";
 
 export default function PreviewScreen({ navigation, route }) {
   const { pahlawan, tipe } = route.params;
-  const data = FotoBerwarna.find((el) => el.title === pahlawan);
-  console.debug(data);
+  // const dataCLR = FotoBerwarna.find((el) => el.title === pahlawan);
+  // const dataHP = FotoHp.find((el) => el.title === pahlawan);
+  // const dataVideo = Video.find((el) => el.title === pahlawan);
+
+  // console.debug(data);
+  const {width} = useWindowDimensions();
   return (
     <View style={styles.container}>
       <Button
@@ -17,6 +21,19 @@ export default function PreviewScreen({ navigation, route }) {
       <Text>
         dengan tipe {tipe} dengan nama pahlawan {pahlawan}
       </Text>
+      {
+            (() => {
+                if(tipe.includes('clr')){
+                    const data = FotoBerwarna.find(el => el.title == pahlawan);
+                    return(<Image source={data.image} style={[styles.image, {width, resizeMode: 'contain'}]} />);
+                }else if(tipe.includes('hp')){
+                    const data = FotoHp.find(el => el.title == pahlawan);
+                    return(<Image source={data.image} style={[styles.image, {width, resizeMode: 'contain'}]} />);
+                }else{
+                    return(<Text>Preview Video Pahlawan {pahlawan}</Text>);
+                }
+            })()
+        }
     </View>
   );
 }
